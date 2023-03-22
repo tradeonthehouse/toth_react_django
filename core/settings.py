0b44d2cf18d771792e82f1699578483050f7ea8e
Vishal Verma <vermavishal891@gmail.com>
2023-03-23 00:24:35 +0530

    npm run build2

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'frontend/build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
