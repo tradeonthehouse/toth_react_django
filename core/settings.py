3e12b05be390b589defcefada10e52d3b56998ca
Vipul mangar <vipsmangar@Vipuls-MacBook-Air.local>
2023-03-25 23:57:10 +0530

    5th

--- core/settings.py
+++ core/settings.py
@@ -84,6 +84,25 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+      'file': {
+         'level': 'DEBUG',
+         'class': 'logging.FileHandler',
+         'filename': '/tmp/debug.log',
+      },
+   },
+   'loggers': {
+      'django': {
+         'handlers': ['file'],
+         'level': 'DEBUG',
+         'propagate': True,
+      },
+   },
+}
+
 WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
