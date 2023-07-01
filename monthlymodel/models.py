f0e61d3c7bf999a1e3ef8c722c375040f8d78070
Vips Mangar <vipsmangar@gmail.com>
2023-07-01 20:09:08 +0530

    fixed dependency

--- monthlymodel/models.py
+++ monthlymodel/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db import models
-from db_file_storage.storage import DatabaseFileStorage
 
 
 class MonthlyDataModel(models.Model):
@@ -58,7 +57,7 @@ class AlertDataModel(models.Model):
 class PositionalDataModel(models.Model):
 
     id = models.AutoField(primary_key = True)
-    Image = models.FileField(storage=DatabaseFileStorage())
+    Image = models.FileField()
     Market_Type = models.TextField()
     Header = models.TextField()
     Description = models.TextField()
